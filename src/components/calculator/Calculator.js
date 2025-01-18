523e5f3a91a93606771ab2a17d713545d70ead30
Simon Anderson <spa0703@gmail.com>
2025-01-18 00:49:20 -0500

    Refactored the calculator logic to check if a number or a operand has been pressed. Began laying out the framework for operations to be performed on a previously entered string of numbers with the current string of numbers.

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,18 +1,55 @@
 "use client";
 
-import CalculatorButton from "./CalculatorButtons";
+import CalculatorButtons from "./CalculatorButtons";
 import { useState } from "react";
 
 const numbers = Array.from(Array(10).keys());
+const operands = ["+", "-"];
+let previousNumbers = "";
+let totalOperandsPressed = 0;
 
-export default function Calculate() {
+export default function Calculator() {
     const [pressedNumbers, setPressedNumbers] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    
+    console.log("L15" + totalOperandsPressed);
+    console.log("Calculator Initiated (Total): " + total);
 
     const handleClick = (event) => {
         setPressedNumbers(pressedNumbers.toString().concat(event.target.value));
+        checkOperand(event);
     }
 
-    console.log(pressedNumbers);
+    function checkOperand(event) {
+        let isValidCase = false;
+
+        switch(event.target.value) {
+            case "+":
+            case "-":
+                isValidCase = true;
+                break;
+            default:
+                isValidCase = false;
+                console.log("No match, defaulting");           
+        }
+        if (isValidCase) {
+            totalOperandsPressed++;
+            console.log(totalOperandsPressed);
+            applyOperand(event);
+        }
+    }
+
+    function applyOperand(event) {
+        //assign previous numbers
+        //reset pressed numbers
+        //increase total operands pressed
+
+        console.log("operand being applied");
+        if (totalOperandsPressed == 2) {
+            totalOperandsPressed = 0;
+        }
+    }
 
     return (
         <div className="flex flex-col justify-center border-2 w-48 mx-auto py-4 rounded-xl bg-zinc-200 border-zinc-600 shadow-xl">
@@ -20,7 +57,7 @@ export default function Calculate() {
                 <h1 className="text-lg text-zinc-200 font-semibold">{pressedNumbers}</h1>
             </div>
             <div className="h-px bg-zinc-700 mb-2" />
-            <CalculatorButton numbers={numbers} onClick={handleClick} />
+            <CalculatorButtons numbers={numbers} onClick={handleClick} />
         </div>
     );
 }
--- src/components/calculator/CalculatorButtons.js
+++ src/components/calculator/CalculatorButtons.js
@@ -1,12 +1,14 @@
 import Button from "../Button"
 
-export default function CalculatorButton({ numbers, onClick }) {
+export default function CalculatorButtons({ numbers, onClick }) {
 
     return (
         <div className="grid grid-cols-3 mx-auto w-40 gap-2">
             {numbers.map((number) => (
                 <Button onClick={onClick} key={number} value={number}>{number}</Button>
             ))}
+            <Button onClick={onClick} value="+">+</Button>
+            <Button onClick={onClick} value="-">-</Button>
         </div>
     )
 }
